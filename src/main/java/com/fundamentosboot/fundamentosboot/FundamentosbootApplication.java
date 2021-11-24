4ea536f4fbeb609c9a38fbe4d76fd76c211665b6
David R <davrodzam@gmail.com>
2021-11-24 08:15:59 -0600

    First commit

---
+++ src/main/java/com/fundamentosboot/fundamentosboot/FundamentosbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosboot.fundamentosboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosboot/fundamentosboot/FundamentosbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosboot.fundamentosboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
